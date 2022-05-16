cf0318bee2ee607a306d10e53de5990567dca09d
OS\ibrlek <ibrlek3005@gmail.com>
2022-05-16 16:33:11 +0200

    ConceptSifarnik

---
+++ BooksStore/Bookstore.RhetosExtensions/Sifarnik.cs
@@ -0,0 +1,40 @@
+﻿using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace Bookstore.RhetosExtensions
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword ("Sifarnik")]
+    public class SifarnikInfo: IConceptInfo
+    {
+        [ConceptKey]
+        public EntityInfo Entity { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class SIfarnikMacro : IConceptMacro<SifarnikInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(SifarnikInfo sifarnik, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+            ShortStringPropertyInfo StringName = new ShortStringPropertyInfo
+            {
+                DataStructure = sifarnik.Entity,
+                Name = "Name"
+            };
+            newConcepts.Add(StringName);
+            newConcepts.Add(new UniquePropertyInfo
+            {
+                Property = StringName
+            });
+            newConcepts.Add(new RequiredPropertyInfo
+            {
+                Property = StringName
+            });
+            return newConcepts;
+        }
+    }
+}
